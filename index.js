1533b1a99c1d576faec44e8e0267e3f0ed87dbae
NovaZer <41429281+Erwanleroy@users.noreply.github.com>
2019-09-24 10:01:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ $("#textInput").keydown(function(e){
     // si la touche press est un entier 
     if(!isNaN(e.key)){
         // si la touche est bien un chiffre on ajoute a la chaine existante notre chiffre
-        if(parseInt($("#textInput").val())<=100 || $("#textInput").val()==""){
+        if(parseInt($("#textInput").val())<=99 || $("#textInput").val()==""){
             $("#textInput").val($("#textInput").val()+e.key)
+        }else if(parseInt($("#textInput").val())===100){
+            if(e.key===0){
+                 $("#textInput").val(1000)
+            }
         }
     }
     if(e.key === "Backspace"){
@@ -23,9 +27,13 @@ $("#textInput").keydown(function(e){
 function fillWithButton(e){
     // Pour empecher de depasser les 1000 brassard =================> parseInt($("#textInput").val())<=100
     // Pour pouvoir commencer a le remplir si linput est vide ======> $("#textInput").val()==""
-    if(parseInt($("#textInput").val())<=100 || $("#textInput").val()==""){
+    if(parseInt($("#textInput").val())<=99 || $("#textInput").val()==""){
         $("#textInput").val($("#textInput").val()+e)
-    }
+    }else if(parseInt($("#textInput").val())===100){
+            if(e==0){
+                 $("#textInput").val(1000)
+            }
+        }
 }
 
 function removeLastNumber(){
@@ -36,4 +44,3 @@ function removeLastNumber(){
 $("document").ready(function(){
     $("#textInput").focus()
 })
-
